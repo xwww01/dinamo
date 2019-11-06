05103978cd46a70abdb1de2081ca9a99509a2735
Samil96 <1628112@utp.edu.pe>
2019-11-06 17:57:05 -0500

    actuali

---
+++ app-dinamo/src/Pages/Asistencia.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+function Asistencia() {
+
+        return (
+            <div className="asistencia">
+                <div className="row">
+                    <div className="col-12">
+                    <h1>Hola</h1> 
+                    </div>
+                </div> 
+                <div className="row">
+                    <div className="col-12">
+
+                    </div>
+                </div>
+            </div>
+          );
+    
+  }
+  
+  export default Asistencia;
